89c09288a1770d72a9b3f22a7251321be51ad074
Fer <ferp@protonmail.ch>
2023-06-23 15:00:06 +0200

    listing available properties from the realEstate contract

--- src/App.js
+++ src/App.js
@@ -1,46 +1,98 @@
-import { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './components/Navigation';
-import Search from './components/Search';
-import Home from './components/Home';
+import Navigation from "./components/Navigation";
+import Search from "./components/Search";
+import Home from "./components/Home";
 
 // ABIs
-import RealEstate from './abis/RealEstate.json'
-import Escrow from './abis/Escrow.json'
+import RealEstate from "./abis/RealEstate.json";
+import Escrow from "./abis/Escrow.json";
 
 // Config
-import config from './config.json';
+import config from "./config.json";
 
 function App() {
-  const [account, setAccount] = useState('');
+  const [account, setAccount] = useState("");
+  const [provider, setProvider] = useState(null);
+  const [escrow, setEscrow] = useState(null);
+  const [homes, setHomes] = useState([]);
+
   const loadBlockchainData = async () => {
-    new ethers.providers.Web3Provider(window.ethereum);
+    const newProvider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(newProvider);
+
+    const network = await newProvider.getNetwork();
+
+    const realEstate = new ethers.Contract(
+      config[network.chainId].realEstate.address,
+      RealEstate,
+      newProvider
+    );
+    const totalSupply = await realEstate.totalSupply();
+    const newHomes = [];
+    for (let i = 1; i <= totalSupply; i++) {
+      const uri = await realEstate.tokenURI(i);
+      const response = await fetch(uri);
+      const metadata = await response.json();
+      newHomes.push(metadata);
+    }
+
+    setHomes(newHomes);
+    console.log(newHomes);
+
+    const newEscrow = new ethers.Contract(
+      config[network.chainId].escrow.address,
+      Escrow,
+      newProvider
+    );
+    setEscrow(newEscrow);
   };
 
   const setAccountChangeListener = async () => {
-    window.ethereum.on('accountsChanged', async () => {
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts'});
+    window.ethereum.on("accountsChanged", async () => {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
       const newAccount = ethers.utils.getAddress(accounts[0]);
       setAccount(newAccount);
-    })
-  }
+    });
+  };
 
   useEffect(() => {
     loadBlockchainData();
     setAccountChangeListener();
-  }, [])
+  }, []);
 
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
-      <div className='cards__section'>
-
-        <h3>Welcome to Millow</h3>
-
+      <Search />
+      <div className="cards__section">
+        <h3>Homes For you</h3>
+        <hr />
+        <div className="cards">
+          {homes.map((home, index) => {
+            return (
+            <div key={home?.id} className="card">
+              <div className="card__image">
+                <img src={home?.image} alt="Home" />
+              </div>
+              <div className="card__info">
+                <h4>{home.attributes[0].value} ETH</h4>
+                <p>
+                  <strong>{home.attributes[2].value}</strong> bds|
+                  <strong>{home.attributes[3].value}</strong> ba|
+                  <strong>{home.attributes[4].value}</strong> sqft
+                </p>
+                <p>{home.address}</p>
+              </div>
+            </div>
+            )
+          })}
+        </div>
       </div>
-
     </div>
   );
 }
